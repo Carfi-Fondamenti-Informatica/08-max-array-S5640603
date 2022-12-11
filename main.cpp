9c672691ec3c5be44ad9161f88be9dbb15012378
S5640603 <115167282+S5640603@users.noreply.github.com>
2022-12-11 14:32:19 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+    int dim=0;
+    cin>>dim;
+    float a[dim];
+    for(int i=0; i<dim; i++){
+        cin>>a[i];
+    }
+    cout<<array(a,dim)<<endl;
+    return 0;
 }
