393757b8d5a70591b8b90110a3d5fc6956bd424f
S5640603 <115167282+S5640603@users.noreply.github.com>
2022-12-11 14:33:00 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
+#include "lib.h"
 
+float array(float a[], int dim){
+    for(int i=0; i<dim; i++){
+        if(a[0]<=a[i]){
+            a[0]=a[i];
+        }else{
+            a[i]=a[0];
+        }
+    }
+    return a[0];
+}
